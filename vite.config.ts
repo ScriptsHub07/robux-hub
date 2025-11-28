e8710c7a2da73daddf9dddb3c940439d94117e2d
script <154348722+ScriptsHub07@users.noreply.github.com>
2025-11-28 13:10:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["robux-hub.onrender.com", "localhost"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
